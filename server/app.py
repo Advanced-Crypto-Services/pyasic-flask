80a413733997eb25dcccbca87272b24ab0dcc12c
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2023-05-11 16:19:46 +0100

    extend scanner, freeze reqs

--- server/app.py
+++ server/app.py
@@ -1,7 +1,48 @@
 import asyncio
+import subprocess
+import json
+import requests
+from requests.auth import HTTPDigestAuth
+from pyasic.network import MinerNetwork
 from pyasic import get_miner
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+
+def ping(host):
+    try:
+        return subprocess.check_output(['fping', '-a', '-q', '-g', host])
+    except subprocess.CalledProcessError as e:
+        # based on https://fping.org/fping.1.html
+        # Exit status is 0 if all the hosts are reachable, 
+        # 1 if some hosts were unreachable, 2 if any IP addresses were not found, 3 for invalid command line arguments, and 4 for a system call failure.
+        if e.returncode == 1:
+             return e.output
+
+        raise RuntimeError('command "{}" return with error (code {}): {}'.format(e.cmd, e.returncode, e.output))
+
+def get_pingable_hosts(host):
+     # return ping(host).split(' ')
+     return ['10.33.13.90', '10.33.13.100', '10.33.13.114', '10.33.13.115', '10.33.13.116', '10.33.13.119', '10.33.13.120', '10.33.13.122', '10.33.13.123', '10.33.13.125', '10.33.13.126', '10.33.13.127', '10.33.13.128', '10.33.13.132', '10.33.13.134', '10.33.13.135', '10.33.13.136']
+
+def get_kernel_log_url(ip, with_auth=False):
+     if not with_auth: 
+          return f'http://{ip}/cgi-bin/get_kernel_log.cgi'
+     else:
+          return f'http://root:root@{ip}/cgi-bin/get_kernel_log.cgi'
+
+def get_kernel_logs(ip):
+     return requests.get(get_kernel_log_url(ip), auth=HTTPDigestAuth('root', 'root'))
+
+def has_kernel_logs(ip):
+    try:
+        resp = get_kernel_logs(ip)
+        return resp.status_code == 200
+    except:
+         return False
+
+def download_kernel_logs(ip):
+     return get_kernel_logs(ip).text
+     
 
 # define asynchronous function to get miner and data
 async def get_miner_data(miner_ip: str):
@@ -12,15 +53,15 @@ async def get_miner_data(miner_ip: str):
     # Get data from the miner
     return await miner.get_data()
 
-from pyasic.network import MinerNetwork
-
-
 # define asynchronous function to scan for miners
 async def scan_and_get_data(ip, mask):
     # Define network range to be used for scanning
     # This can take a list of IPs, a constructor string, or an IP and subnet mask
     # The standard mask is /24 (x.x.x.0-255), and you can pass any IP address in the subnet
     net = MinerNetwork(ip, mask=mask)
+
+    hosts = list(map(lambda ip: str(ip), list(net.hosts())) )
+
     # Scan the network for miners
     # This function returns a list of miners of the correct type as a class
     miners: list = await net.scan_network_for_miners()
@@ -31,15 +72,21 @@ async def scan_and_get_data(ip, mask):
     # Gather all tasks asynchronously and run them
     results = await asyncio.gather(*tasks)
 
-    return results
+    # return a list of dicts, without datetime (not json serializable)
+    def to_clean_dict(l):
+        r = l.asdict()
+        r.pop('datetime')
+        return r
+
+    return list(map(to_clean_dict, results))
 
 app = Flask(__name__)
 CORS(app)
-cors = CORS(app, resources={r"/miner": {"origins": "*"}, r"/scan": {"origins": "*"}})
+cors = CORS(app, resources={r'/miner': {'origins': '*'}, r'/scan': {'origins': '*'}})
 
 @app.route('/miner', methods=['GET'])
 async def miner():
-    data = await get_miner_data(request.args.get("ip"))
+    data = await get_miner_data(request.args.get('ip'))
     response = app.response_class(
         response=data.as_json(),
         status=200,
@@ -50,15 +97,25 @@ async def miner():
 
 @app.route('/scan', methods=['GET'])
 async def scan():
-    miners = await scan_and_get_data(request.args.get("ip"), request.args.get("mask", default=24, type=int))
+    ip = request.args.get('ip')
+    mask = request.args.get('mask', default=24, type=int)
 
-    result = "["
+    miners = await scan_and_get_data(ip, mask)
+    pingable_ips = get_pingable_hosts(f'{ip}/{mask}')
+
+    # identify IPs that are pingable but are NOT identified as working miners
+    potential_miners = list(filter(lambda ip: has_kernel_logs(ip), list(set(pingable_ips) - set(map(lambda m: m['ip'], miners)))))
+
+    results = {}
 
     for miner in miners:
-        result += f'{miner.as_json()},'
+         results[miner['ip']] = { 'miner_data': miner, 'kernel_log': f'{request.host_url}kernel-logs?ip={miner["ip"]}' }
+
+    for potential_miner in potential_miners:
+         results[potential_miner] = { 'kernel_log': f'{request.host_url}kernel-logs?ip={miner["ip"]}' }
 
     response = app.response_class(
-        response=f'{result.strip(",")} ]',
+        response=json.dumps(results),
         status=200,
         mimetype='application/json'
     )
@@ -72,6 +129,10 @@ def info():
 		'host': request.headers['Host'],
 		'user-agent': request.headers['User-Agent']
 	})
+
+@app.route('/kernel-logs', methods=['GET'])
+def kernel_logs():
+	return download_kernel_logs(request.args.get('ip'))
 
 @app.route('/flask-health-check')
 def flask_health_check():
